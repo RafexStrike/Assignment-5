ccb776ec7eda8b89da0ca9b182449c145933a72a
RafexStrike <adnanrahmanrafi515@gmail.com>
2025-03-02 06:06:51 +0600

    random color changing feature done

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// Select the h3 element
 const dateElement = document.getElementById('date');
-
-// Get the current date
 const today = new Date();
 
 // Format the date as "Wed, Jul 28 2025"
@@ -13,5 +10,31 @@ const options = {
 };
 const formattedDate = today.toLocaleDateString('en-US', options).replace(/(\d+)/, '$1 '); // Adds space before day
 
-// Set the text content of the h3
 dateElement.textContent = formattedDate;
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// Function to generate a random hex color
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+const body = document.getElementById("body")
+
+const themeButton = document.getElementById("themeButton");
+themeButton.addEventListener("click", ()=>{
+    let randomColor = getRandomColor()
+    console.log(randomColor)
+    body.style.backgroundColor = randomColor
+})
